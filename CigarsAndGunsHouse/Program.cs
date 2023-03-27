c4245b5b9bee4bd613016d0b973e1de62319a150
Steffen Mogensen <feber.sm@gmail.com>
2023-03-27 21:06:34 +0200

    first check-in

---
+++ CigarsAndGunsHouse/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CigarsAndGunsHouse
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            SocketServer server = new SocketServer(9001);
+            server.Start();
+        }
+    }
+}
